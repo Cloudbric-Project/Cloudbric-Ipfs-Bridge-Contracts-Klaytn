1c97b45229bf48c40cf48c572dc210f8ffb33815
Hyunwoo Lee <aeharvlee@gmail.com>
2019-10-04 22:51:40 +0900

    Fix: minor bugs
    * fix bug after deleting cloudbricIpfsBridgeAuth contract.
    * and add some stuff to make code more readable and useful.

--- test/test.js
+++ test/test.js
@@ -1,27 +1,19 @@
-const fs = require('fs');
+const caverConfig = require('../config/caver');
 const helper = require('../helper/helper');
 
-const secret = fs.readFileSync(".secret.json").toString();
-const parsedSecret = JSON.parse(secret);
-const LOCAL_URL = parsedSecret.local.URL;
+const caver = caverConfig.caver;
 
-const Caver = require('caver-js');
-const caver = new Caver(LOCAL_URL);
-
+const WhiteList = artifacts.require("WhiteList.sol");
 const CloudbricIpfsBridge = artifacts.require("CloudbricIpfsBridge.sol");
-const CloudbricIpfsBridgeAuth = artifacts.require("CloudbricIpfsBridgeAuth");
+
+let cloudbricIpfsBridge = null;
+let whiteList = null;
+
+let whiteSpace  = '     ';
 
 let owner = null;
 let alice = null;
 let bob = null;
-
-let cib = null;
-
-// dummy data set
-let wafBlackIpIndex = null; 
-let wafBlackIpHash = null;
-
-let whiteSpace  = '     ';
 
 contract("Setup For Each Testcase", async accounts => {
     beforeEach("setup accounts and contract for each test", async () => {
@@ -29,65 +21,73 @@ contract("Setup For Each Testcase", async accounts => {
         alice = accounts[1];
         bob = accounts[2];
 
-        cib = await CloudbricIpfsBridge.deployed();
+        whiteList = await WhiteList.deployed();
+        cloudbricIpfsBridge = await CloudbricIpfsBridge.deployed();
+    });
+
+    it("owner of White List should be set correctly", async () => {
+        const ownerOfWhiteList = await whiteList.owner.call();
+        assert.equal(ownerOfWhiteList, owner);
     });
 
     it("owner of Cloudbric Ipfs Bridge should be set correctly", async () => {
-        let ownerOfCib = cib.owner();
-        assert.equal(ownerOfCib, ownerOfCib);
+        const ownerOfCloudbricIpfsBridge = await cloudbricIpfsBridge.owner.call();
+        assert.equal(ownerOfCloudbricIpfsBridge, owner); 
     });
 });
 
-contract("Cloudbric Ipfs Bridge:: waf Black Ips test", async accounts => {
-    it("addWafBlackIp should be failed when executed by not owner", async () => {
-        let { idxWafBlackIpList, wafBlackIpHash, hashFunction, size } = helper.createDummy(1);
+contract("WhiteList:: authentication test", async accounts => {
+    it("addWhiteList must be executed by owner", async () => {
         try {
-            let result = await cib.addWafBlackIp(
-                idxWafBlackIpList,
-                wafBlackIpHash,
-                hashFunction,
-                size,
-                { from: alice }
-            );
-            console.log(result);
-        } catch (e) {
-            let message = `${alice} is not not allowed to execute function.`;
-            assert.fail(
-                e.reason
-                + '\n'
-                + whiteSpace
-                + message
-            );
+            await whiteList.addWhiteList(bob, {from: owner});
+        } catch (error) {
+            assert.fail(error);
         }
+        assert.ok(true);
     });
 
-    it("addWafBlackIp should be failed when data already exists", async () => {
-        let { idxWafBlackIpList, wafBlackIpHash, hashFunction, size } = helper.createDummy(1);
+    it("addWhiteList must be failed when exectued by not owner", async () => {
         try {
-            let result = await cib.addWafBlackIp(
-                idxWafBlackIpList,
-                wafBlackIpHash,
-                hashFunction,
-                size,
+            await whiteList.addWhiteList(bob, {from: alice});
+        } catch (error) {
+            assert.ok(true);
+        }
+    });
+})
+
+contract("Cloudbric Ipfs Bridge:: waf Black Ips test", async accounts => {
+    beforeEach("setup dummy dataset", async () => {
+        let dummy = helper.createDummy(0);
+        dummy = helper.encodeDataSet(dummy);
+    });
+
+    it("addWafBlackIp must be executed by owner", async () => {
+        try {
+            await cloudbricIpfsBridge.addWafBlackIp(
+                dummy.encodedIdxWafBlackIpList,
+                dummy.encodedWafBlackIpHash,
+                dummy.encodedHashFunction,
+                dummy.encodedSize,
                 { from: owner }
             );
-            console.log(result);
-        } catch (e) {
-            let message = `${wafBlackIpIndex} is already exists.`;
-            assert.fail(
-                e.reason
-            );
+            assert.ok(true);
+        } catch (error) {
+            assert.fail(error);
         }
     });
 
-    it("getWafBalckIpAtIndex should work correctly", async () => {
+    it("addWafBlackIp must be failed when executed by not owner", async () => {
         try {
-            let result = await cib.getWafBlackIpAtIndex(
-                hackerWalletIndex,
-                { from: owner });
-            
+            await cloudbricIpfsBridge.addWafBlackIp(
+                dummy.encodedIdxWafBlackIpList,
+                dummy.encodedWafBlackIpHash,
+                dummy.encodedHashFunction,
+                dummy.encodedSize,
+                { from: alice }
+            );
         } catch (e) {
-            console.log(e);
+            assert.ok(true);
         }
     });
+
 });
