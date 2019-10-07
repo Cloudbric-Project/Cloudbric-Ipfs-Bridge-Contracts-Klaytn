203b5401f137090abac9d9a71cb8dce9ae002a25
Hyunwoo Lee <aeharvlee@gmail.com>
2019-10-08 00:38:35 +0900

    Add: basic ipfs add batch process

--- ipfs-bridge.js
+++ ipfs-bridge.js
@@ -1,9 +1,23 @@
+const fs = require('fs');
 
 const ipfsClient = require('ipfs-http-client');
-// const ipfs = ipfsClient('/ip4/<ip_address>/tcp/5001/');
-
+const ipfs = ipfsClient('/ip4/172.105.229.53/tcp/5001/'); 
 // logging process setup
+const INITIAL_INSERTED_IDX = 210511986;
 
-async function threatdbInsert() {
+async function wafBlackIpInsert(startIdx, limit) {
+    for (let i = 0; i < limit; i++) {
+        let idx = startIdx + i;
+        const wafBlackIpJson = fs.readFileSync(`./data/waf_black_ip/${idx}.json`).toString();
+        const bufferedWafBlackIp = Buffer.from(wafBlackIpJson);
 
+        try {
+            const wafBlackIpAdded = await ipfs.add(bufferedWafBlackIp, {pin: true});
+            console.log(wafBlackIpAdded);
+        } catch (error) { 
+            console.log(error);
+        }
+    }
 }
+
+wafBlackIpInsert(INITIAL_INSERTED_IDX, 100);
