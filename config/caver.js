be1056a5300da779e4a30cde87ec0f6028c17989
Hyunwoo Lee <aeharvlee@gmail.com>
2019-10-08 10:47:17 +0900

    Delete: secret file and merge this content to caver
    * reduced useless complexity.

--- config/caver.js
+++ config/caver.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+// direcotry path setup
+const appDir = path.dirname('index.js');
+
+const caverSecret = fs.readFileSync(`${appDir}/private/.caver.json`).toString();
+const vault = JSON.parse(caverSecret);
+
 const Caver = require('caver-js');
-const secret = require('./secret');
-const vault = secret.vault;
 
 const caver = new Caver(vault.local.URL);
 
--- config/secret.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const secret = fs.readFileSync(".secret.json").toString();
-const parsedSecret = JSON.parse(secret);
-
-module.exports = {
-    vault: parsedSecret
-}
